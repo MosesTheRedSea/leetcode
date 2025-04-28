ef2a2ebc16c13d09d7e003f981f6291ab7312fdd
Moses Adewolu <MosesOluwatobiAdewolu@gmail.com>
2025-04-27 21:15:18 -0400

    new changes

--- solutions/easy/C++/twoSum.cpp
+++ solutions/easy/C++/twoSum.cpp
@@ -32,15 +32,34 @@
 #include <cstdint> // for fast and least types
 using namespace std;
 
-
 /*
 	█▀▄▀█ █▀▀█ █▀▀ █▀▀ █▀▀ ─█▀▀█ █▀▀▄ █▀▀ █───█ █▀▀█ █── █──█ 
 	█░█░█ █──█ ▀▀█ █▀▀ ▀▀█ ░█▄▄█ █──█ █▀▀ █▄█▄█ █──█ █── █──█ 
 	█──░█ ▀▀▀▀ ▀▀▀ ▀▀▀ ▀▀▀ ░█─░█ ▀▀▀─ ▀▀▀ ─▀─▀─ ▀▀▀▀ ▀▀▀ ─▀▀▀
 */
 
-int main() {
-	return 0;
+class Solution {
+public:
+    vector<int> twoSumLoop(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+        	for (int j = i + 1; j < nums.size(); ++j) {
+        		if (nums[i] + nums[j] == target) {
+        			return vector<int>{nums[i], nums[j]};
+        		}
+        	}
+        }
+        return vector<int>{-1, -1};
+    }
+    vector<int> twoSumMap(vector<int>& nums, int target) {
+    }
+};
+
+
+vector<int> findAnswers(vector<int>& nums, int target) {
+
 }
 
-
+int main() {
+	
+	return 0;
+}
